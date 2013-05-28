1a02fd4aa808155fb32d6884091d61d5bf2c03b4
Paul van Assen <source@paules.nl>
2013-05-28 09:27:37 +0200

    Fixed RAG numbers only

--- src/test/java/org/paules/geckoboard/api/widget/FunnelGraphTest.java
+++ src/test/java/org/paules/geckoboard/api/widget/FunnelGraphTest.java
@@ -12,37 +12,36 @@ import org.paules.geckoboard.api.type.GraphType;
 
 public class FunnelGraphTest {
 
-	@Test
-	public void testJson() throws JsonProcessingException, IOException {
-		FunnelGraph widget = new FunnelGraph("1234", GraphType.STANDARD, false);
-		widget.addData("Step 1", "100");
-		widget.addData("Step 2", "200");
-		widget.addData("Step 3", "300");
-		widget.addData("Step 4", "500");
-		widget.addData("Step 5", "10 mega");
-		widget.toJson();
-		ObjectMapper om = new ObjectMapper();
-		JsonNode node = om.readTree(widget.toJson());
-		JsonNode data = node.get("data");
-		assertEquals("standard", data.get("type").asText());
-		assertEquals("hide", data.get("percentage").asText());
+    @Test
+    public void testJson() throws JsonProcessingException, IOException {
+        FunnelGraph widget = new FunnelGraph( "1234", GraphType.STANDARD, false );
+        widget.addData( "Step 1", "100" );
+        widget.addData( "Step 2", "200" );
+        widget.addData( "Step 3", "300" );
+        widget.addData( "Step 4", "500" );
+        widget.addData( "Step 5", "10 mega" );
 
-		assertEquals("Step 1", data.get("item").get(0).get("label").asText());
-		assertEquals("100", data.get("item").get(0).get("value").asText());
+        ObjectMapper om = new ObjectMapper();
+        JsonNode node = om.readTree( widget.toJson() );
+        JsonNode data = node.get( "data" );
+        assertEquals( "standard", data.get( "type" ).asText() );
+        assertEquals( "hide", data.get( "percentage" ).asText() );
 
-		assertEquals("Step 2", data.get("item").get(1).get("label").asText());
-		assertEquals("200", data.get("item").get(1).get("value").asText());
+        assertEquals( "Step 1", data.get( "item" ).get( 0 ).get( "label" ).asText() );
+        assertEquals( "100", data.get( "item" ).get( 0 ).get( "value" ).asText() );
 
-		assertEquals("Step 3", data.get("item").get(2).get("label").asText());
-		assertEquals("300", data.get("item").get(2).get("value").asText());
+        assertEquals( "Step 2", data.get( "item" ).get( 1 ).get( "label" ).asText() );
+        assertEquals( "200", data.get( "item" ).get( 1 ).get( "value" ).asText() );
 
-		assertEquals("Step 4", data.get("item").get(3).get("label").asText());
-		assertEquals("500", data.get("item").get(3).get("value").asText());
+        assertEquals( "Step 3", data.get( "item" ).get( 2 ).get( "label" ).asText() );
+        assertEquals( "300", data.get( "item" ).get( 2 ).get( "value" ).asText() );
 
-		assertEquals("Step 5", data.get("item").get(4).get("label").asText());
-		assertEquals("10 mega", data.get("item").get(4).get("value").asText());
+        assertEquals( "Step 4", data.get( "item" ).get( 3 ).get( "label" ).asText() );
+        assertEquals( "500", data.get( "item" ).get( 3 ).get( "value" ).asText() );
 
-		
-	}
+        assertEquals( "Step 5", data.get( "item" ).get( 4 ).get( "label" ).asText() );
+        assertEquals( "10 mega", data.get( "item" ).get( 4 ).get( "value" ).asText() );
+
+    }
 
 }
---
+++ src/test/java/org/paules/geckoboard/api/widget/RAGNumbersOnlyTest.java
@@ -0,0 +1,35 @@
+package org.paules.geckoboard.api.widget;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+public class RAGNumbersOnlyTest {
+
+    @Test
+    public void testJson() throws JsonProcessingException, IOException {
+        RAGNumbersOnly ragNumbersOnly = new RAGNumbersOnly( "1234" );
+        ragNumbersOnly.setRed( "Test-red", 123 );
+        ragNumbersOnly.setAmber( "Test-amber", 1234 );
+        ragNumbersOnly.setGreen( "Test-green", 12345 );
+        
+        ObjectMapper om = new ObjectMapper();
+        JsonNode node = om.readTree( ragNumbersOnly.toJson() );
+        
+        assertEquals(3,node.get("item").size());
+        assertEquals("Test-red", node.get("item").get( 0 ).get( "text" ).asText());
+        assertEquals(123, node.get("item").get( 0 ).get( "value" ).asInt());
+
+        assertEquals("Test-amber", node.get("item").get( 1 ).get( "text" ).asText());
+        assertEquals(1234, node.get("item").get( 1 ).get( "value" ).asInt());
+        
+        assertEquals("Test-green", node.get("item").get( 2 ).get( "text" ).asText());
+        assertEquals(12345, node.get("item").get( 2 ).get( "value" ).asInt());
+    }
+
+}
