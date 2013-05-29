28efe721d1eec4632d84692ada42429122734510
Paul van Assen <source@paules.nl>
2013-05-29 15:30:29 +0200

    Added text tests and fixes

--- src/test/java/org/paules/geckoboard/api/widget/RAGNumbersOnlyTest.java
+++ src/test/java/org/paules/geckoboard/api/widget/RAGNumbersOnlyTest.java
@@ -17,19 +17,19 @@ public class RAGNumbersOnlyTest {
         ragNumbersOnly.setRed( "Test-red", 123 );
         ragNumbersOnly.setAmber( "Test-amber", 1234 );
         ragNumbersOnly.setGreen( "Test-green", 12345 );
-        
+
         ObjectMapper om = new ObjectMapper();
         JsonNode node = om.readTree( ragNumbersOnly.toJson() );
-        
-        assertEquals(3,node.get("item").size());
-        assertEquals("Test-red", node.get("item").get( 0 ).get( "text" ).asText());
-        assertEquals(123, node.get("item").get( 0 ).get( "value" ).asInt());
 
-        assertEquals("Test-amber", node.get("item").get( 1 ).get( "text" ).asText());
-        assertEquals(1234, node.get("item").get( 1 ).get( "value" ).asInt());
-        
-        assertEquals("Test-green", node.get("item").get( 2 ).get( "text" ).asText());
-        assertEquals(12345, node.get("item").get( 2 ).get( "value" ).asInt());
+        assertEquals( 3, node.get( "item" ).size() );
+        assertEquals( "Test-red", node.get( "item" ).get( 0 ).get( "text" ).asText() );
+        assertEquals( 123, node.get( "item" ).get( 0 ).get( "value" ).asInt() );
+
+        assertEquals( "Test-amber", node.get( "item" ).get( 1 ).get( "text" ).asText() );
+        assertEquals( 1234, node.get( "item" ).get( 1 ).get( "value" ).asInt() );
+
+        assertEquals( "Test-green", node.get( "item" ).get( 2 ).get( "text" ).asText() );
+        assertEquals( 12345, node.get( "item" ).get( 2 ).get( "value" ).asInt() );
     }
 
 }
