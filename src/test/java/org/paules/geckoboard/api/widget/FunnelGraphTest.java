4ee5476ed67df55036beb1ee5633a85a5d15c459
Paul van Assen <source@paules.nl>
2013-06-01 17:20:24 +0200

    Fixed tests

--- src/test/java/org/paules/geckoboard/api/widget/FunnelGraphTest.java
+++ src/test/java/org/paules/geckoboard/api/widget/FunnelGraphTest.java
@@ -8,7 +8,7 @@ import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
-import org.paules.geckoboard.api.type.GraphType;
+import org.paules.geckoboard.api.json.GraphType;
 
 public class FunnelGraphTest {
 
@@ -23,24 +23,23 @@ public class FunnelGraphTest {
 
         ObjectMapper om = new ObjectMapper();
         JsonNode node = om.readTree( widget.toJson() );
-        JsonNode data = node.get( "data" );
-        assertEquals( "standard", data.get( "type" ).asText() );
-        assertEquals( "hide", data.get( "percentage" ).asText() );
+        assertEquals( "standard", node.get( "type" ).asText() );
+        assertEquals( "hide", node.get( "percentage" ).asText() );
 
-        assertEquals( "Step 1", data.get( "item" ).get( 0 ).get( "label" ).asText() );
-        assertEquals( "100", data.get( "item" ).get( 0 ).get( "value" ).asText() );
+        assertEquals( "Step 1", node.get( "item" ).get( 0 ).get( "label" ).asText() );
+        assertEquals( "100", node.get( "item" ).get( 0 ).get( "value" ).asText() );
 
-        assertEquals( "Step 2", data.get( "item" ).get( 1 ).get( "label" ).asText() );
-        assertEquals( "200", data.get( "item" ).get( 1 ).get( "value" ).asText() );
+        assertEquals( "Step 2", node.get( "item" ).get( 1 ).get( "label" ).asText() );
+        assertEquals( "200", node.get( "item" ).get( 1 ).get( "value" ).asText() );
 
-        assertEquals( "Step 3", data.get( "item" ).get( 2 ).get( "label" ).asText() );
-        assertEquals( "300", data.get( "item" ).get( 2 ).get( "value" ).asText() );
+        assertEquals( "Step 3", node.get( "item" ).get( 2 ).get( "label" ).asText() );
+        assertEquals( "300", node.get( "item" ).get( 2 ).get( "value" ).asText() );
 
-        assertEquals( "Step 4", data.get( "item" ).get( 3 ).get( "label" ).asText() );
-        assertEquals( "500", data.get( "item" ).get( 3 ).get( "value" ).asText() );
+        assertEquals( "Step 4", node.get( "item" ).get( 3 ).get( "label" ).asText() );
+        assertEquals( "500", node.get( "item" ).get( 3 ).get( "value" ).asText() );
 
-        assertEquals( "Step 5", data.get( "item" ).get( 4 ).get( "label" ).asText() );
-        assertEquals( "10 mega", data.get( "item" ).get( 4 ).get( "value" ).asText() );
+        assertEquals( "Step 5", node.get( "item" ).get( 4 ).get( "label" ).asText() );
+        assertEquals( "10 mega", node.get( "item" ).get( 4 ).get( "value" ).asText() );
 
     }
 
--- src/test/java/org/paules/geckoboard/api/widget/LineChartTest.java
+++ src/test/java/org/paules/geckoboard/api/widget/LineChartTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.awt.Color;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.JsonProcessingException;
@@ -21,8 +22,8 @@ public class LineChartTest {
         lineChart.addDataPoint( "4" );
         lineChart.addDataPoint( "0.4" );
         lineChart.setColor( Color.RED );
-        lineChart.setXAxisLabels( new String[] { "Jan", "Feb", "Mar", "Apr" } );
-        lineChart.setYAxisLabels( new String[] { "1", "2", "3", "4", "5" } );
+        lineChart.setXAxisLabels( Arrays.asList( new String[] { "Jan", "Feb", "Mar", "Apr" } ) );
+        lineChart.setYAxisLabels( Arrays.asList( new String[] { "1", "2", "3", "4", "5" } ) );
 
         ObjectMapper om = new ObjectMapper();
         JsonNode node = om.readTree( lineChart.toJson() );
@@ -33,20 +34,22 @@ public class LineChartTest {
         assertEquals( "4", node.get( "item" ).get( 2 ).asText() );
         assertEquals( "0.4", node.get( "item" ).get( 3 ).asText() );
 
-        assertTrue( node.get( "settings" ).get( "xaxis" ).isArray() );
-        assertEquals( 4, node.get( "settings" ).get( "xaxis" ).size() );
-        assertEquals( "Jan", node.get( "settings" ).get( "xaxis" ).get( 0 ).asText() );
-        assertEquals( "Feb", node.get( "settings" ).get( "xaxis" ).get( 1 ).asText() );
-        assertEquals( "Mar", node.get( "settings" ).get( "xaxis" ).get( 2 ).asText() );
-        assertEquals( "Apr", node.get( "settings" ).get( "xaxis" ).get( 3 ).asText() );
+        assertTrue( node.get( "settings" ).get( "axisx" ).isArray() );
+        assertEquals( 4, node.get( "settings" ).get( "axisx" ).size() );
+        assertEquals( "Jan", node.get( "settings" ).get( "axisx" ).get( 0 ).asText() );
+        assertEquals( "Feb", node.get( "settings" ).get( "axisx" ).get( 1 ).asText() );
+        assertEquals( "Mar", node.get( "settings" ).get( "axisx" ).get( 2 ).asText() );
+        assertEquals( "Apr", node.get( "settings" ).get( "axisx" ).get( 3 ).asText() );
 
-        assertTrue( node.get( "settings" ).get( "yaxis" ).isArray() );
-        assertEquals( 5, node.get( "settings" ).get( "yaxis" ).size() );
-        assertEquals( "1", node.get( "settings" ).get( "yaxis" ).get( 0 ).asText() );
-        assertEquals( "2", node.get( "settings" ).get( "yaxis" ).get( 1 ).asText() );
-        assertEquals( "3", node.get( "settings" ).get( "yaxis" ).get( 2 ).asText() );
-        assertEquals( "4", node.get( "settings" ).get( "yaxis" ).get( 3 ).asText() );
-        assertEquals( "5", node.get( "settings" ).get( "yaxis" ).get( 4 ).asText() );
+        assertTrue( node.get( "settings" ).get( "axisy" ).isArray() );
+        assertEquals( 5, node.get( "settings" ).get( "axisy" ).size() );
+        assertEquals( "1", node.get( "settings" ).get( "axisy" ).get( 0 ).asText() );
+        assertEquals( "2", node.get( "settings" ).get( "axisy" ).get( 1 ).asText() );
+        assertEquals( "3", node.get( "settings" ).get( "axisy" ).get( 2 ).asText() );
+        assertEquals( "4", node.get( "settings" ).get( "axisy" ).get( 3 ).asText() );
+        assertEquals( "5", node.get( "settings" ).get( "axisy" ).get( 4 ).asText() );
+
+        assertEquals( "FF0000FF", node.get( "settings" ).get( "colour" ).asText() );
     }
 
 }
