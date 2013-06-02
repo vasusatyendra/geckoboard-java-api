e8901eed79058cc563906a35ec8f6ddd526b6bd7
Paul van Assen <source@paules.nl>
2013-06-02 11:34:16 +0200

    POM cleanup

--- src/main/java/org/paules/geckoboard/api/json/RAGColor.java
+++ src/main/java/org/paules/geckoboard/api/json/RAGColor.java
@@ -1,21 +1,17 @@
 package org.paules.geckoboard.api.json;
 
-import com.google.gson.annotations.SerializedName;
 
 public enum RAGColor {
     /**
      * Red color
      */
-    @SerializedName( "red" )
     RED,
     /**
      * Amber color
      */
-    @SerializedName( "amber" )
     AMBER,
     /**
      * Green color
      */
-    @SerializedName( "green" )
     GREEN;
 }
