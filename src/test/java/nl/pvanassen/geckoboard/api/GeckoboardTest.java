441904ea6cd78f6bdb86ff8da0fe515cd6633fb2
Paul van Assen <source@paules.nl>
2014-07-10 21:32:04 +0200

    Too high Duh

--- src/test/java/nl/pvanassen/geckoboard/api/GeckoboardTest.java
+++ src/test/java/nl/pvanassen/geckoboard/api/GeckoboardTest.java
@@ -19,7 +19,7 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 public class GeckoboardTest {
 
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(68574);
+    public WireMockRule wireMockRule = new WireMockRule(2020);
 
     @Test(expected = IllegalArgumentException.class)
     public void testGeckoboardEmpty() {
@@ -47,7 +47,7 @@ public class GeckoboardTest {
                                 .withBody("<response>Some content</response>")));
 
         Geckoboard geckoboard = new Geckoboard("test");
-        geckoboard.setBaseUrl("http://localhost:68574/");
+        geckoboard.setBaseUrl("http://localhost:2020/");
         geckoboard.push(new MockedPush());
         /*
          * assertTrue(result.wasSuccessFul());
